78c77d22dd0c754a9ca9a2ce84952e0d638caed8
Barnabas Gonda <barnabasgonda@Barnabass-iMac.local>
2023-05-27 15:23:20 +0200

    player

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  currentCard: string | undefined = '';
+  game: Game = new Game();
 
-takeCard() {
-  this.pickCardAnimation = true;
-}
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  constructor() {
+
+  }
+
+  newGame() {
+    this.game;
+    console.log(this.game);
+  }
+
+
+  takeCard() {
+
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+      console.log(this.currentCard);
+    
+
+     setTimeout(() => {
+      if (this.currentCard) {
+        this.game.playedCards.push(this.currentCard);
+        console.log(this.game.playedCards);
+      }
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
+
+
+  }
 
 }
